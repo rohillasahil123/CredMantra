f6a0f1cd95e69b1baa6ec4e83a3680e3551d8a21
sahil <rohillasahil704@gmail.com>
2024-11-01 12:15:09 -0700

    updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <button className=' font-bold text-xl ml-4 border w-[100px] bg-yellow-400  h-7  rounded-lg hover:bg-yellow-700 hover:cursor-pointer'>Login</button>
         <div className='font-bold text-2xl ml-8 cursor-pointer '> <Link to='/home'> <span className='text-red-300'>  LO</span><span className='text-sky-300'>GO</span> </Link> </div>
         <div className='flex space-x-6 font-medium mr-7 items-center '>
-        <div className='cursor-pointer' > <Link to='' ></Link> Our Team</div>
+        <div className='cursor-pointer' > <Link to='/ourteam' >Our Team </Link></div>
         <div className='cursor-pointer'>Blog</div>
         </div>
     </div>
--- src/Components/OurTeam/Ourteam.jsx
+++ src/Components/OurTeam/Ourteam.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import oneImg from "../../assets/on.png"
 import twoImg from "../../assets/te.png"
-import Partner from '../Partners/Partner'
+import fastApproval from '../../assets/loanfast.webp'
+import wildproject from '../../assets/wideproject.png'
+import documents from '../../assets/document.png'
+import process from '../../assets/pocess.png'
+import speed from '../../assets/speed.png'
+import secure from '../../assets/safty.png'
+
 
 const Ourteam = () => {
   return (
@@ -53,12 +59,41 @@ const Ourteam = () => {
           </p>
         </div>    
         </div>
-        <h1 className='text-[60px] font-bold text-orange-700'>What <span className='text-sky-300'>Makes </span> Us <span className='text-sky-600'>Special </span> </h1>
-        <p className="text-xl font-semibold">
+        <h1 className='text-[30px] sm:text-[60px] font-bold text-orange-700'>What <span className='text-sky-300'>Makes </span> Us <span className='text-sky-600'>Special </span> </h1>
+        <p className=" ">
         Our offerings shine with their competitive interest rates and user-friendly processes, liberating our customers from the constraints of traditional, convoluted financial procedures.
           </p>
+          <h1 className='text-xl font-bold'>
+          "At Credmantra, we believe financial empowerment is the cornerstone of progress. We’re here to help you build, manage, and grow your wealth with confidence."</h1>
       </div>
-      <Partner/>
+      <div className='flex h-[90vh] sm:h-[50vh] w-full justify-center sm:justify-around flex-col sm:flex-row mt-[2%] items-center space-y-3 sm:space-y-0'>
+        <div className='shadow-2xl  h-[80%] sm:h-[80%] w-[90%]  sm:w-[26%] items-center text-center content-center rounded-lg hover:cursor-pointer transition-transform hover:scale-105 '>
+          <img src={fastApproval} alt="" srcset=""  className='h-[70%] sm:h-[50%] w-[60%] ml-[20%]'/>
+          <h1 className='font-bold text-2xl '>Fast approval rate</h1>
+        </div>
+        <div className='shadow-2xl  h-[80%] sm:h-[80%] w-[90%]  sm:w-[26%] items-center text-center content-center rounded-lg hover:cursor-pointer transition-transform hover:scale-105 '>
+          <img src={process} alt="" srcset=""  className='h-[70%] sm:h-[50%] w-[60%] ml-[20%]'/>
+          <h1 className='font-bold text-2xl '>100% paperless process</h1>
+        </div>
+        <div className='shadow-2xl  h-[80%] sm:h-[80%] w-[90%]  sm:w-[26%] items-center text-center content-center rounded-lg hover:cursor-pointer transition-transform hover:scale-105 '>
+          <img src={documents} alt="" srcset=""  className='h-[70%] sm:h-[50%] w-[60%] ml-[20%]'/>
+          <h1 className='font-bold text-2xl '>Hassle free documentation</h1>
+        </div>
+      </div>
+      <div className='flex h-[90vh] sm:h-[50vh] w-full justify-center sm:justify-around flex-col sm:flex-row mt-[1%] items-center space-y-3 sm:space-y-0'>
+        <div className='shadow-2xl  h-[80%] sm:h-[80%] w-[90%]  sm:w-[26%] items-center text-center content-center rounded-lg hover:cursor-pointer transition-transform hover:scale-105 '>
+          <img src={speed} alt="" srcset=""  className='h-[70%] sm:h-[50%] w-[60%] ml-[20%]'/>
+          <h1 className='font-bold text-2xl '>Fast disbursal</h1>
+        </div>
+        <div className='shadow-2xl  h-[80%] sm:h-[80%] w-[90%]  sm:w-[26%] items-center text-center content-center rounded-lg hover:cursor-pointer transition-transform hover:scale-105 '>
+          <img src={wildproject} alt="" srcset=""  className='h-[70%] sm:h-[50%] w-[60%] ml-[20%]'/>
+          <h1 className='font-bold text-2xl '>Wild project rate</h1>
+        </div>
+        <div className='shadow-2xl  h-[80%] sm:h-[80%] w-[90%]  sm:w-[26%] items-center text-center content-center rounded-lg hover:cursor-pointer transition-transform hover:scale-105 '>
+          <img src={secure} alt="" srcset=""  className='h-[70%] sm:h-[50%] w-[60%] ml-[20%]'/>
+          <h1 className='font-bold text-2xl '>Safe data ecosystem</h1>
+        </div>
+      </div>
       </>
   )
 }
