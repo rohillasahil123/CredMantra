aee20697d43c040798ca44574d7b4e543ab0b48a
sahil <rohillasahil704@gmail.com>
2024-11-08 04:31:04 -0800

    updated

--- src/Components/LanderList/LanderList.jsx
+++ src/Components/LanderList/LanderList.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { GiMoneyStack } from "react-icons/gi";
 
-const LanderList = () => {
+const LenderList = () => {
   return (
-    <div>
-        <div>
-            <div>
-                <h1 className='text-4xl font-semibold'><span></span> Select a Lander</h1>
-                <p></p>
-            </div>
+    <div className='text-center items-center' style={{justifyItems:'center'}}>
+        <div className='flex items-center text-4xl font-semibold justify-center '>
+            <GiMoneyStack /> 
+            <h1 className='mx-2'>Select a Lender</h1> 
+            <GiMoneyStack />   <br />
         </div>
+        <p className='text-2xl mt-[2%]'>Here are the offers that will best suits Your need.</p>
+
+    <div className='border w-[93%] h-[40vh] mt-[4%] sm::mt-[2%] shadow-2xl'>
+        h1
+
+    </div>
+
+
     </div>
   )
 }
 
-export default LanderList
+export default LenderList
