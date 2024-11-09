799d60c641b869889210e55fff0abf41dcd5d4d5
sahil <rohillasahil704@gmail.com>
2024-11-09 08:11:10 -0800

    updated

--- src/Components/FixBox/Fixbox.jsx
+++ src/Components/FixBox/Fixbox.jsx
@@ -36,40 +36,42 @@ const Fixbox = () => {
   }, []);
 
   return (
-    isVisible && (
-      <div className="fixed sm:h-[16%] h-[12%] bottom-0  left-0  w-[100%] sm:w-[100%] bg-orange-300 rounded-md z-50 ease-in-out">
-        <div className="h-full border rounded-lg w-full  shadow-2xl space-y-0 sm:space-y-3   text-center">
-          <h1 className="font-semibold text-xl">
-            Get Instant Loan Approval in less than 2 minutes
-          </h1>
-          {isOnScreen ? (
-            <input
-              type="text"
-              placeholder="OTP"
-              className="border h-9 w-[60%] rounded-md  ml-3"
-              onChange={handleChange}
-              value={otp}
-              name="otp"
-            />
-          ) : (
-            <input
-              type="text"
-              placeholder="91+ Phone Number"
-              className="border h-9 w-[50%] rounded-md ml-3 "
-              onChange={handleChange}
-              value={phoneNumber}
-              name="phoneNumber"
-            />
-          )}
-          <button
-            className="h-9 w-[20%] sm:w-[15%] ml-[3%] border rounded-md bg-teal-800 text-white font-bold"
-            onClick={handelOtpSend}
-          >
-            Send OTP
-          </button>
-        </div>
+    <div
+      className={`fixed bottom-0 left-0 w-full sm:h-[18%] h-[12%] bg-orange-300 rounded-md z-50 ease-in-out transition-all duration-500 transform ${
+        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-full"
+      }`}
+    >
+      <div className="h-full border rounded-lg w-full shadow-2xl space-y-0 sm:space-y-3 text-center p-4">
+        <h1 className="font-semibold text-xl">
+          Get Instant Loan Approval in less than 2 minutes
+        </h1>
+        {isOnScreen ? (
+          <input
+            type="text"
+            placeholder="OTP"
+            className="border h-9 w-[60%] rounded-md ml-3"
+            onChange={handleChange}
+            value={otp}
+            name="otp"
+          />
+        ) : (
+          <input
+            type="text"
+            placeholder="+91 9991462406"
+            className="border h-9 w-[50%] rounded-md ml-3"
+            onChange={handleChange}
+            value={phoneNumber}
+            name="phoneNumber"
+          />
+        )}
+        <button
+          className="h-9 w-[20%] sm:w-[15%] ml-[3%] border rounded-md bg-teal-800 text-white font-bold"
+          onClick={handelOtpSend}
+        >
+          Send OTP
+        </button>
       </div>
-    )
+    </div>
   );
 };
 
