04cb7e803b382ea8e5c36e0e8326ad808cd59af2
sahil <rohillasahil704@gmail.com>
2025-01-06 12:45:55 +0530

    updatedaos

--- src/Components/HomeAll/choose/Choose.jsx
+++ src/Components/HomeAll/choose/Choose.jsx
@@ -33,24 +33,23 @@ const Choose = () => {
             <div className="flex flex-col md:flex-row text-center w-full  h-auto">
                 <div className="flex flex-col items-center bg-sky-300 w-full md:w-1/2 p-4">
                     <div className="mt-10 space-y-4 sm:space-y-2" >
-                        <div 
-                             className="bg-white h-[45%] mt-[17%] sm:mr-[20%] w-max sm:w-full rounded-lg shadow-lg flex flex-col items-center mr-[30%] p-2 hover:cursor-pointer transition-transform hover:scale-105"
-                             data-aos={window.innerWidth >= 768 ? "fade-right" : "fade-up"}
-                             data-aos-delay="200"
+                    <div 
+                        className="bg-white h-[45%] mt-[10%] sm:ml-[35%] w-max sm:w-full rounded-lg shadow-lg flex flex-col items-center mr-[30%] p-2 hover:cursor-pointer transition-transform hover:scale-105"
+                            data-aos={window.innerWidth >= 768 ? "fade-right" : "fade-up"}
+                            data-aos-delay="300"
                         >
-                            <div className="rounded-full bg-white h-[60px] flex mt-3 mx-auto w-[50px] justify-center shadow-2xl">
+                            <div className="rounded-full bg-white h-[60px] flex mt-3 mx-auto w-[60px] justify-center shadow-2xl">
                                 <img src={clock} alt="Fast Disbursal" className="w-full h-full" />
                             </div>
                             <h1 className="text-xl font-semibold">Fast Disbursal</h1>
-                            <p>Our team helps customers <br /> to make informed loan decisions.</p>
+                            <p>Our team helps customers <br /> make informed loan decisions.</p>
                         </div>
                         <div 
-                        
-                        className="bg-white h-[45%] mt-[17%] sm:ml-[20%] w-max sm:w-full rounded-lg shadow-lg flex flex-col items-center mr-[30%] p-2 hover:cursor-pointer transition-transform hover:scale-105"
-                        data-aos={window.innerWidth >= 768 ? "fade-right" : "fade-up"} 
-                        data-aos-delay="200"
+                        className="bg-white h-[45%] mt-[10%] sm:ml-[35%] w-max sm:w-full rounded-lg shadow-lg flex flex-col items-center mr-[30%] p-2 hover:cursor-pointer transition-transform hover:scale-105"
+                            data-aos={window.innerWidth >= 768 ? "fade-right" : "fade-up"}
+                            data-aos-delay="300"
                         >
-                            <div className="rounded-full bg-white ml-0  sm:ml-[20%] h-[60px] flex mt-3 mx-auto w-[60px] justify-center shadow-2xl">
+                            <div className="rounded-full bg-white h-[60px] flex mt-3 mx-auto w-[60px] justify-center shadow-2xl">
                                 <img src={clock} alt="Fast Disbursal" className="w-full h-full" />
                             </div>
                             <h1 className="text-xl font-semibold">Fast Disbursal</h1>
